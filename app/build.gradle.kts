31e850d337bddcc96cf6effe7b69f39b277860d9
Iqbal Fauzan <iqbalmf68@gmail.com>
2023-07-18 17:09:58 +0700

    add dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,15 +1,19 @@
 plugins {
     id ("com.android.application")
     id ("org.jetbrains.kotlin.android")
+    id ("androidx.navigation.safeargs.kotlin")
+    id ("dagger.hilt.android.plugin")
+    id ("kotlin-kapt")
 }
-
+@Suppress("UnstableApiUsage")
 android {
-    compileSdk = 31
+    compileSdk = 33
+    namespace = "net.iqbalfauzan.myfoodyapp"
 
     defaultConfig {
-        applicationId = "net.iqbalfauzan.myfoody"
+        applicationId = "net.iqbalfauzan.myfoodyapp"
         minSdk = 23
-        targetSdk = 31
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
@@ -29,6 +33,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -36,8 +43,64 @@ dependencies {
     implementation ("androidx.core:core-ktx:1.7.0")
     implementation ("androidx.appcompat:appcompat:1.4.1")
     implementation ("com.google.android.material:material:1.5.0")
-    implementation ("androidx.constraintlayout:constraintlayout:2.1.3")
+    implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation ("junit:junit:4.13.2")
     androidTestImplementation ("androidx.test.ext:junit:1.1.3")
     androidTestImplementation ("androidx.test.espresso:espresso-core:3.4.0")
+
+    // Coordinator Layout
+    implementation ("androidx.coordinatorlayout:coordinatorlayout:1.2.0")
+
+    // Material Components
+    implementation ("com.google.android.material:material:1.6.0-alpha01")
+
+    // Navigation Component
+    implementation ("androidx.navigation:navigation-fragment-ktx:2.5.2")
+    implementation ("androidx.navigation:navigation-ui-ktx:2.5.2")
+
+    // Room components
+    implementation ("androidx.room:room-runtime:2.3.0")
+    kapt ("androidx.room:room-compiler:2.3.0")
+    implementation ("androidx.room:room-ktx:2.3.0")
+    androidTestImplementation ("androidx.room:room-testing:2.3.0")
+
+    // DataStore
+    implementation ("androidx.datastore:datastore-preferences:1.0.0")
+
+    // Recyclerview
+    implementation ("androidx.recyclerview:recyclerview:1.3.0")
+
+    // Retrofit
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    //Dagger - Hilt
+    implementation ("com.google.dagger:hilt-android:2.44")
+    kapt ("com.google.dagger:hilt-android-compiler:2.44")
+
+    implementation ("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03")
+    kapt ("androidx.hilt:hilt-compiler:1.0.0")
+
+    // Coroutines
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.0")
+
+    // Lifecycle
+    implementation ("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.0")
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.4.0")
+
+    // Image Loading library Coil
+    implementation ("io.coil-kt:coil:0.13.0")
+
+    // Gson
+    implementation ("com.google.code.gson:gson:2.8.6")
+
+    // Shimmer
+    implementation ("com.facebook.shimmer:shimmer:0.5.0")
+//    implementation 'com.todkars:shimmer-recyclerview:0.4.0'
+
+    // Jsoup
+    implementation ("org.jsoup:jsoup:1.13.1")
 }
